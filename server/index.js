fbae70b8305f1e9ca03118fc1117df411457e049
Al366io <alessio.nannipieri0@gmail.com>
2023-01-10 18:40:55 +0100

    index

--- server/index.js
+++ server/index.js
@@ -1,17 +1,31 @@
-const Sequelize = require('sequelize');
-sequelize = new Sequelize(
-  'postgresql://localhost:5432/frameitdb',
-  {
-    logging: false
-  }
-);
+const express = require("express");
+const cors = require("cors");
+const router = require("./router");
+const app = express();
+const port = 3001;
+const http = require("http");
+const server = http.createServer(app);
+const socketIo = require("socket.io");
+global.io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+}); //in case server and client run on different urls
 
-async function start() {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection to db ok');
-  } catch (error) {
-    console.log('err' + error);
-  }
-} start();
-module.exports = sequelize;
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+io.on("connection", (socket) => {
+  console.log("client connected: ", socket.id);
+  socket.on("join-room", (room) => {
+    socket.join(room);
+  });
+  socket.on("disconnect", (reason) => {
+    console.log(reason);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port} - http://localhost:${port}`);
+});
