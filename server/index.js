446c36613d635275dd81821b58ac10cc1e0a0ca3
Al366io <alessio.nannipieri0@gmail.com>
2023-01-10 18:39:02 +0100

    minor

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+sequelize = new Sequelize(
+  'postgresql://localhost:5432/frameitdb',
+  {
+    logging: false
+  }
+);
+
+async function start() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to db ok');
+  } catch (error) {
+    console.log('err' + error);
+  }
+} start();
+module.exports = sequelize;
