2bf0a70c61fe8d2bb0e8ea8e132eda7966ff3d6f
Al366io <alessio.nannipieri0@gmail.com>
2023-01-11 18:27:42 +0100

    minorx2

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 // import { useState } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
 import Navbar from '../components/Navbar';
@@ -7,6 +8,7 @@ import '../styles/Dashboard.css';
 
 function Dashboard() {
   const { id } = useParams();
+  const { isAuthenticated } = useAuth0();
   // const [copied, setCopied] = useState(false);
 
   // const handleCopyLink = () => {
@@ -16,13 +18,15 @@ function Dashboard() {
 
   function handleShare() {
     if (navigator.share) {
-      navigator.share({
-        title: 'FrameIt - Room',
-        url: `https://frame-it.vercel.app/dashboard/${id}`
-      }).then(() => {
-        console.log('Thanks for sharing!');
-      })
-      .catch(console.error);
+      navigator
+        .share({
+          title: 'FrameIt - Room',
+          url: `https://frame-it.vercel.app/dashboard/${id}`,
+        })
+        .then(() => {
+          console.log('Thanks for sharing!');
+        })
+        .catch(console.error);
     } else {
       console.log('nooo');
     }
@@ -31,18 +35,22 @@ function Dashboard() {
   return (
     <div className="dashboardWrapper">
       <Navbar></Navbar>
-      <div className="qrWrap">
-        <h3 className="removeDefaultStyling">Room #{id}</h3>
-        <QRCodeSVG
-          id="dash-qr"
-          size="130px"
-          value={`http://localhost:3000/adder/${id}`}
-        />
-        <button className="buttonShare" onClick={handleShare}>
-          SHARE
-        </button>
-        <span></span>
-      </div>
+      {isAuthenticated ? (
+        <div className="qrWrap">
+          <h3 className="removeDefaultStyling">Room #{id}</h3>
+          <QRCodeSVG
+            id="dash-qr"
+            size="130px"
+            value={`http://localhost:3000/adder/${id}`}
+          />
+          <button className="buttonShare" onClick={handleShare}>
+            SHARE
+          </button>
+          <span></span>
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   );
 }
