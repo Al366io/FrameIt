d34e0ecb5398e8f6bd074846ceb1b098850ab420
Al366io <alessio.nannipieri0@gmail.com>
2023-01-11 17:59:08 +0100

    home when click logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
+
+  function handleGohome() {
+    navigate(`/`);
+  } 
 
   return (
     <div className="navbar">
       <div className="logo">
         <img
+          onClick={handleGohome}
           className="logoImg"
           src={process.env.PUBLIC_URL + '/assets/frameit.png'}
           alt="img"
