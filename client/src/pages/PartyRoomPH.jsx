c5fb1f09c503b161a74db24185809e277db863fe
Al366io <alessio.nannipieri0@gmail.com>
2023-01-12 19:05:43 +0100

    input button correct

--- client/src/pages/PartyRoomPH.jsx
+++ client/src/pages/PartyRoomPH.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from 'react-router-dom';
 import { QRCodeSVG } from 'qrcode.react';
 import Navbar from '../components/Navbar';
 import '../styles/Dashboard.css';
-import Camera from "react-html5-camera-photo";
-import "react-html5-camera-photo/build/css/index.css";
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 import ImagePreview from '../components/ImagePreview';
 
 // reachable at /party/:id/ph/add
@@ -15,7 +15,7 @@ function PartyRoomPH() {
   const { id } = useParams();
   const [dataUri, setDataUri] = useState('');
   const [openCamera, setOpenCamera] = useState(false);
-
+  const [fileUploaded, setFileUploaded] = useState(false)
   const handleTakePhoto = async (dataUri) => {
     // if (dataUri) {
     //   setPhoto(dataUri);
@@ -33,24 +33,43 @@ function PartyRoomPH() {
     setOpenCamera(!openCamera);
   }
 
+  function handleChange() {
+    const input = document.getElementById('foto')
+    let buff = input.files[0]
+    if(buff) {
+      setFileUploaded(true)
+    } else setFileUploaded(false)
+  }
+
   return (
     <div className="dashboardWrapper">
       <div className="firstHalf">
         {' '}
         ROOM #{id}
-        {/* <button onClick={toggleCamera} className="logButton">
-          Open Camera 📸
-        </button> */}
-        <form method="post" enctype="multipart/form-data" id="formSend" onSubmit={sendIt}>
-          <div className='fotoWrap'>
-          <label for="foto" class="customFoto">
-            📸
-          </label>
-          <label>Click to take Photo</label>
+        <form
+          method="post"
+          encType="multipart/form-data"
+          id="formSend"
+          onSubmit={sendIt}
+        >
+          <div className="fotoWrap">
+            <label for="foto" className="customFoto">
+              📸
+            </label>
+            <label>Click to take Photo</label>
           </div>
-          <input id="foto" type="file" name="image" capture="enviroment" accept="image/*"/>
-          <button className="logButton" type="submit">SEND</button>
-          {/* <button class="apriCamera" onclick={document.getElementById('foto').click()}> 📷 </button> */}
+          <input
+            id="foto"
+            type="file"
+            name="image"
+            capture="enviroment"
+            accept="image/*"
+            onChange={handleChange}
+          />
+          { fileUploaded ? 
+          <button className="logButton" type="submit">
+            SEND
+          </button> : ''}
         </form>
       </div>
     </div>
