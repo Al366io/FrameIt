efd781ee25041d6b976a6c04f736c3112c4e8c77
Al366io <alessio.nannipieri0@gmail.com>
2023-01-15 16:44:30 +0100

    styling

--- client/src/pages/NoPage.jsx
+++ client/src/pages/NoPage.jsx
@@ -8,7 +8,7 @@ function NoPage() {
     <Navbar />
     <div id="errorContainer">
       <h3 id="errorHead">404</h3>
-      <h3 id="errorSubhead">You're looking for the wrong queue</h3>
+      <h3 id="errorSubhead">You're looking for the wrong party</h3>
       <Link to='/' id="errorBackHome">Go Back Home ?</Link>
     </div>
     </>
