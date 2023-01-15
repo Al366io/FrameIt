025864a4751fc041aa7e1fecdfc8eb2031cc5598
Al366io <alessio.nannipieri0@gmail.com>
2023-01-15 10:55:43 +0100

    bg color

--- client/src/ApiServices.js
+++ client/src/ApiServices.js
@@ -21,7 +21,7 @@ export async function createOwner(user_email) {
       },
       body: JSON.stringify(data),
     }).then((response) => response);
-    console.log(response.status);
+    // console.log(response.status);
     if (response.status == 204) 
       return 1
     else
